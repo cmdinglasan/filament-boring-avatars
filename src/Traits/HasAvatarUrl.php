10fa9cdcc83fce5469068b2c0d5884cfa3649946
cmdinglasan <cmdinglasan@users.noreply.github.com>
2022-10-02 11:41:11 +0000

    Fix styling

--- src/Traits/HasAvatarUrl.php
+++ src/Traits/HasAvatarUrl.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 
 trait HasAvatarUrl
 {
-	public function avatarUrl(): Attribute
-	{
-		return new Attribute(
-			get: fn () => app()->make(BoringAvatars::class)->get(
-				$this->name,
-				config('filament-boring-avatars.variant', 'marble'),
-				config('filament-boring-avatars.size', 40),
-				config('filament-boring-avatars.colors', []),
-				config('filament-boring-avatars.url', 'https://source.boringavatars.com')
-			)
-		);
-	}
+    public function avatarUrl(): Attribute
+    {
+        return new Attribute(
+            get: fn () => app()->make(BoringAvatars::class)->get(
+                $this->name,
+                config('filament-boring-avatars.variant', 'marble'),
+                config('filament-boring-avatars.size', 40),
+                config('filament-boring-avatars.colors', []),
+                config('filament-boring-avatars.url', 'https://source.boringavatars.com')
+            )
+        );
+    }
 }
